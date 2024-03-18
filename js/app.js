6783397f54444a8e42ad4b9f8ab76c974e7b7340
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-18 14:01:42 +0100

    ADD: Milestone 0

---
+++ js/app.js
@@ -0,0 +1,48 @@
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+const arrayOurTeam = [
+    {
+        name : 'Wayne Barnett',
+        role : 'Founder & CEO',
+        profileImg : 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name : 'Angela Caroll',
+        role : 'Chief editor',
+        profileImg : 'angela-caroll-chief-editor.jpg', 
+    },
+    {
+        name : 'Walter Gordon',
+        role : 'Office Manager',
+        profileImg : 'walter-gordon-office-manager.jpg', 
+    },
+    {
+        name : 'Angela Lopez',	
+        role : 'Social Media Manager',
+        profileImg : 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name : 'Scott Estrada',	
+        role : 'Developer',
+        profileImg : 'scott-estrada-developer.jpg',
+    },
+    {
+        name : 'Barbara Ramos',	
+        role : 'Graphic Designer',
+        profileImg : 'barbara-ramos-graphic-designer.jpg',
+    }
+
+
+]
+
+console.log(arrayOurTeam);
+
+
+// DATI:
+// Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+// Angela Lopez	    Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	            scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
+
